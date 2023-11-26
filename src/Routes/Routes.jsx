7acec3d5cd8e4f0d6c11e2f81cdb0ef5a0af086e
ohidursohag <ohidursohag9595@gmail.com>
2023-11-26 15:01:50 +0600

    implemented Error Page

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -4,11 +4,13 @@ import Home from "../Pages/Home/Home";
 import Login from "../Pages/Login/Login";
 import Register from "../Pages/Register/Register";
 import DashboardLayout from "../Layouts/DashboardLayout";
+import ErrorPage from "../Pages/Error/ErrorPage";
 
 const myCreatedRoutes = createBrowserRouter([
    {
       path: '/',
       element: <MainLayout />,
+      errorElement: <ErrorPage/>,
       children: [
          {index: true, element: <Home />},
       ]
